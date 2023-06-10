223b1fb240a992a6c6e3f5f7bd857ca7b252eecd
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2023-06-10 23:50:21 +0530

    ,

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${process.env.NEXT_PUBLIC_PUBLIC_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_PUBLIC_KEY}`;
   const fetchData = (e) => {
     e.preventDefault();
     setLoading(true);
@@ -26,8 +26,10 @@ export default function Home() {
       <Head>
         <title>WeatherApp</title>
       </Head>
-      <h1>tareun</h1>
-      <button onClick={fetchData}>click here</button>
+      <img
+        className="obje"
+        src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1075&q=80"
+      ></img>
     </>
   );
 }
