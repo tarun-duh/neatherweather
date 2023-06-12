00600e353d123151d7147ddb2ebd25b0a08eb238
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2023-06-12 22:50:44 +0530

    .

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  let imageUrl =
+    "https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80";
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body
+        style={{
+          background: `url(${imageUrl})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          height: "100vh",
+        }}
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -26,10 +26,7 @@ export default function Home() {
       <Head>
         <title>WeatherApp</title>
       </Head>
-      <img
-        className="obje"
-        src="https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1075&q=80"
-      ></img>
+      <h1 className="font-bold text-5xl">tarun</h1>
     </>
   );
 }
