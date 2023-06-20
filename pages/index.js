86daef941e51856f541f9a5e72d713403839e278
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2023-06-20 21:34:05 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,8 @@ export default function Home() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_PUBLIC_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.NEXT_PUBLIC_PUBLIC_KEY}`;
+
   const fetchData = (e) => {
     e.preventDefault();
     setLoading(true);
@@ -27,17 +28,29 @@ export default function Home() {
         <title>WeatherApp</title>
       </Head>
       <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/40 z-[1]"></div>
-      //search//
-      <div className="relative flex ">
-        <form>
+      {/*search*/}
+      <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10 ">
+        <form
+          onSubmit={fetchData}
+          className="flex justify-between items-center w-full m-auto p-3 bg-transparent border-white border text-white rounded-2xl
+        "
+        >
           <div>
-            <input type="text" placeholder="search city" name="" id="" />
+            <input
+              onChange={(e) => setCity(e.target.value)}
+              className="bg-transparent border-none text-white focus:outline-none text-xl "
+              type="text"
+              placeholder="search city"
+              name=""
+              id=""
+            />
           </div>
           <button onClick={fetchData}>
-            <BsSearch></BsSearch>
+            <BsSearch size={20}></BsSearch>
           </button>
         </form>
       </div>
+      {/*weather */}
     </>
   );
 }
